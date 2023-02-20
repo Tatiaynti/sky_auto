1e88fe795fe5dcccc2b6b4d25406b0f9ac6c2718
Tatiaynti <tatianamo1234@mail.ru>
2023-02-20 22:33:04 +0300

    HW5

---
+++ lesson_5/task_5.py
@@ -0,0 +1,17 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.maximize_window()
+
+driver.get("http://uitestingplayground.com/classattr")
+
+btn_locator = 'button.class1'
+btn_element = driver.find_element(By.CSS_SELECTOR, btn_locator)
+
+btn_element.click()
+
+sleep(5)
